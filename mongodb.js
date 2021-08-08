83c0f9b9e19a6b29439f5d9e1eb7e7169cd3d89e
Rythem Datta <ritzy192@gmail.com>
2021-08-08 17:41:11 +0530

    created new collection task, inserted documents

--- mongodb.js
+++ mongodb.js
@@ -8,8 +8,41 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     if(error)
         return console.log('Unable to Connect to the database!')
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'Rythem Datta',
-        Age: 24
+    // db.collection('users').insertOne({
+    //     name: 'Rythem Datta',
+    //     Age: 24
+    // })
+    // db.collection('users').insertMany([
+    //     {
+    //         name: "Raman",
+    //         age: 20
+    //     },
+    //     {
+    //         name: "Aman",
+    //         age: 17
+    //     }
+    // ], (error, result) => {
+    //     if(error)
+    //         return console.log('Unable to inser records')
+    //     console.log(result.insertedIds)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Task 1',
+            complete: true
+        },
+        {
+            description: 'Task 2',
+            complete: false
+        },
+        {
+            description: 'Task 3',
+            complete: true
+        }
+    ], (error, result) => {
+        if(error)
+            return console.log('Unable to insert records.')
+        console.log(result.acknowledged)
     })
 })
