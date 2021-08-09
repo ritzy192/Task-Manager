54b3b9aa680ab3d6b24e11c95210d2ad1b3253c4
Rythem Datta <ritzy192@gmail.com>
2021-08-09 19:57:26 +0530

    read data using findOne and find methods

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
 
+
+const {MongoClient, ObjectID} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
@@ -8,41 +8,40 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     if(error)
         return console.log('Unable to Connect to the database!')
     const db = client.db(databaseName)
-    // db.collection('users').insertOne({
-    //     name: 'Rythem Datta',
-    //     Age: 24
-    // })
-    // db.collection('users').insertMany([
-    //     {
-    //         name: "Raman",
-    //         age: 20
-    //     },
-    //     {
-    //         name: "Aman",
-    //         age: 17
-    //     }
-    // ], (error, result) => {
+    
+    //read:: fetch from users collection(table) using findone() using query object passing key value.
+    db.collection('users').findOne({name: 'Rythem Datta', age: 24}, (error, user) => {
+        if(error)
+            return console.log('Unable to fetch data.')
+        console.log(user)
+    })
+    
+    // //findone using object Id.
+    // db.collection('users').findOne({"_id": ObjectId("610fc0ce0cfbc1db62fe3273")}, (error, user) => {
     //     if(error)
-    //         return console.log('Unable to inser records')
-    //     console.log(result.insertedIds)
+    //         return console.log('Unable to fetch data.')
+    //     console.log(user)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Task 1',
-            complete: true
-        },
-        {
-            description: 'Task 2',
-            complete: false
-        },
-        {
-            description: 'Task 3',
-            complete: true
-        }
-    ], (error, result) => {
+    db.collection('users').find({age: 20}).toArray((error, users) =>{
+        //console.log(users)
+    }) 
+
+    db.collection('tasks').findOne({description: 'Task 1'}, (error, task) => {
         if(error)
-            return console.log('Unable to insert records.')
-        console.log(result.acknowledged)
+            return console.log('Unable to fetch data.')
+        //console.log(task)
     })
+    //var o_id = new MongoClient.ObjectID("610fc83684e1ed18a92a432c");
+    db.collection('tasks').findOne({ _id: ObjectID("610fc83684e1ed18a92a432c") }, (error, task) => {
+        if(error)
+            return console.log('Unable to fetch data.')
+        console.log(task)
+    })
+
+
+    db.collection('tasks').find({complete: true}).toArray((error, tasks) =>{
+        //console.log(tasks)
+    })
+
 })
