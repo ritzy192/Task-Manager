fc2c6fff4b93f506ba265090aa173314b34f5bba
Rythem Datta <ritzy192@gmail.com>
2021-08-11 17:14:02 +0530

    UpdateMany and updateOne used

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 
 
-const {MongoClient, ObjectID} = require('mongodb')
+const {MongoClient, ObjectId} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
@@ -8,40 +8,49 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     if(error)
         return console.log('Unable to Connect to the database!')
     const db = client.db(databaseName)
+
+    // const updatePromise = db.collection('users').updateOne({
+    //     _id: ObjectId("610f9d3172356c96ee568e37")
+    // }, {
+    //     $set: {
+    //         name: 'Rohit'
+    //     }
+    // })
+    // updatePromise.then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
     
-    //read:: fetch from users collection(table) using findone() using query object passing key value.
-    db.collection('users').findOne({name: 'Rythem Datta', age: 24}, (error, user) => {
+    //chaining the prommise, smaller than the above method.
+    db.collection('users').updateOne({
+        _id: ObjectId("610f9d3172356c96ee568e37")
+        }, {
+            $inc: {
+                age: 1
+            }
+        }).then((result) => {
+            console.log(result)
+        }).catch((error) => {
+            console.log(error)
+        })
+
+    db.collection('users').findOne({"_id": ObjectId("610f9d3172356c96ee568e37")}, (error, user) => {
         if(error)
             return console.log('Unable to fetch data.')
         console.log(user)
     })
-    
-    // //findone using object Id.
-    // db.collection('users').findOne({"_id": ObjectId("610fc0ce0cfbc1db62fe3273")}, (error, user) => {
-    //     if(error)
-    //         return console.log('Unable to fetch data.')
-    //     console.log(user)
-    // })
 
-    db.collection('users').find({age: 20}).toArray((error, users) =>{
-        //console.log(users)
-    }) 
-
-    db.collection('tasks').findOne({description: 'Task 1'}, (error, task) => {
-        if(error)
-            return console.log('Unable to fetch data.')
-        //console.log(task)
-    })
-    //var o_id = new MongoClient.ObjectID("610fc83684e1ed18a92a432c");
-    db.collection('tasks').findOne({ _id: ObjectID("610fc83684e1ed18a92a432c") }, (error, task) => {
-        if(error)
-            return console.log('Unable to fetch data.')
-        console.log(task)
-    })
-
-
-    db.collection('tasks').find({complete: true}).toArray((error, tasks) =>{
-        //console.log(tasks)
-    })
-
+    db.collection('tasks').updateMany({
+        completed: false
+    },{
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })   
 })
