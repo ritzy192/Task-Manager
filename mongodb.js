9064725f7cbb962178412e7654f134de83f3ad1f
Rythem Datta <ritzy192@gmail.com>
2021-08-11 17:37:38 +0530

    used deleteMany, deleteOne, conclude CRUD using node

--- mongodb.js
+++ mongodb.js
@@ -9,48 +9,19 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         return console.log('Unable to Connect to the database!')
     const db = client.db(databaseName)
 
-    // const updatePromise = db.collection('users').updateOne({
-    //     _id: ObjectId("610f9d3172356c96ee568e37")
-    // }, {
-    //     $set: {
-    //         name: 'Rohit'
-    //     }
-    // })
-    // updatePromise.then((result) => {
-    //     console.log(result)
-    // }).catch((error) => {
-    //     console.log(error)
-    // })
-
-    
-    //chaining the prommise, smaller than the above method.
-    db.collection('users').updateOne({
-        _id: ObjectId("610f9d3172356c96ee568e37")
-        }, {
-            $inc: {
-                age: 1
-            }
-        }).then((result) => {
-            console.log(result)
-        }).catch((error) => {
-            console.log(error)
-        })
-
-    db.collection('users').findOne({"_id": ObjectId("610f9d3172356c96ee568e37")}, (error, user) => {
-        if(error)
-            return console.log('Unable to fetch data.')
-        console.log(user)
-    })
-
-    db.collection('tasks').updateMany({
-        completed: false
-    },{
-        $set: {
-            completed: true
-        }
+    db.collection('users').deleteMany({
+        age: 20
     }).then((result) => {
         console.log(result)
     }).catch((error) => {
         console.log(error)
-    })   
+    })
+    
+    db.collection('tasks').deleteOne({
+        description: 'Task 3'
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
 })
