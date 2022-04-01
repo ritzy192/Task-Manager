4f75818c25a7b3595683e36c1e719c1ef896faa3
Datta <2096688@cognizant.com>
2022-04-01 16:19:27 +0530

    created read all and read one APIs for users and tasks

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 //to automatically parse incoming json into object
 app.use(express.json())
 
-//Post User
+//Create Users
 app.post("/users", (req,res) =>{
     //initialise new user
     const user = new User(req.body)
@@ -22,19 +22,57 @@ app.post("/users", (req,res) =>{
     })
 })
 
-//POST Task
+//Read all Users
+app.get('/users',(req,res) => {
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
+
+//Read one user
+app.get('/users/:id',(req,res) => {
+    User.findById(req.params.id).then((user)=>{
+        if(!user)
+            return res.status(404).send()
+       res.status(200).send(user) 
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+//Create Tasks
 app.post("/tasks", (req,res) =>{
     //initialise new task
-    const task = new Task(req.body)
-    
+    const task = new Task(req.body)    
     //save task into database
     task.save().then(() => {
-        res.status(201).send(task)
+        res.status(200).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
 })
 
+//Read all Tasks
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{
+        if(!task)
+            return res.status(404).send()
+        res.status(200).send(tasks)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+//Read One Task
+app.get('/tasks/:id',(req,res)=>{
+    Task.findById(req.params.id).then((task)=>{
+        res.send(task)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
 app.listen(port,() => {
     console.log('Server is up on port ' + port)
 })
