b8293e9f03eaca688d4aa8c7bf173f125297ed3b
Datta <2096688@cognizant.com>
2022-04-01 14:48:12 +0530

    created task model and built create task API

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,15 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manger-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-// const Tasks = mongoose.model('Tasks', {
-//     Description: {
-//         type: String,
-//         trim: true,
-//         required: true
-//     }, 
-//     Completed: {
-//         type: Boolean,
-//         default: false
-//     }
-// })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,16 +9,29 @@ const port = process.env.PORT || 3000
 //to automatically parse incoming json into object
 app.use(express.json())
 
+//Post User
 app.post("/users", (req,res) =>{
     //initialise new user
     const user = new User(req.body)
     
     //save user into database
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
-        res.status(400)
-        res.send(e)
+        res.status(400).send(e)
+    })
+})
+
+//POST Task
+app.post("/tasks", (req,res) =>{
+    //initialise new task
+    const task = new Task(req.body)
+    
+    //save task into database
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
     })
 })
 
