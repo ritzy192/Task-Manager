6ccff474f851c0a2d220fd921354038a956d073f
Datta <2096688@cognizant.com>
2022-04-05 23:20:32 +0530

    implemented async await in promise-chaining.js file

--- src/playground/promise-chaining.js
+++ src/playground/promise-chaining.js
@@ -1,16 +1,31 @@
 require("../db/mongoose");
 const User = require("../models/user");
 
-User.findByIdAndUpdate("624553622d32983a7071e76c", { age: 44 })
-  .then(() => {
-    console.log("reached");
-    return User.find({ age: 44 });
-  })
-  .then((count) => {
-    console.log("reached 2");
-    console.log(count[0].age);
-  })
-  .catch((e) => {
-    console.log("reached 3");
+const updateAgeThenCount = async (id,age)=>{
+  const user = await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments({ age });
+  const details = {
+    user,count
+  }
+  return details
+}
+
+updateAgeThenCount('624553622d32983a7071e76c',44).then((details) => {
+    console.log(details)
+  }).catch((e) => {
     console.log(e);
   });
+
+// User.findByIdAndUpdate("624553622d32983a7071e76c", { age: 44 })
+//   .then(() => {
+//     console.log("reached");
+//     return User.find({ age: 44 });
+//   })
+//   .then((count) => {
+//     console.log("reached 2");
+//     console.log(count[0].age);
+//   })
+//   .catch((e) => {
+//     console.log("reached 3");
+//     console.log(e);
+//   });
