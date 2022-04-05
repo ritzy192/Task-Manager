65e5f3cdaf7f9b54e2cff713b8114b6d6311c905
Datta <2096688@cognizant.com>
2022-04-05 23:35:19 +0530

    implemented async-await in promise-chaining-2.js file

--- src/playground/promise-chaining-2.js
+++ src/playground/promise-chaining-2.js
@@ -1,11 +1,24 @@
 require('../db/mongoose')
 const Task = require('../models/task')
 
-Task.findByIdAndDelete('624c33589ebb404534d94d4b').then((task)=>{
-    console.log(task)
-    return Task.countDocuments({completed:false})   
-}).then((count)=>{
-    console.log(count)
+const deleteTaskAndCount = async (id)=>{
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed:true})
+    return {task,count}
+}
+
+deleteTaskAndCount('624c335e9ebb404534d94d4d').then((details)=>{
+    console.log(details)
 }).catch((e)=>{
-    console.log(e)
+    console.log('*******ERWERWERWERWER**********', e)
 })
+
+
+// Task.findByIdAndDelete('624c33589ebb404534d94d4b').then((task)=>{
+//     console.log(task)
+//     return Task.countDocuments({completed:false})   
+// }).then((count)=>{
+//     console.log(count)
+// }).catch((e)=>{
+//     console.log(e)
+// })
