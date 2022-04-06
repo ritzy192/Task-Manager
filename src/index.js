822989447ae4c01a0775a8d99573374e3880cbf5
Datta <2096688@cognizant.com>
2022-04-06 20:47:34 +0530

    created update user API

--- src/index.js
+++ src/index.js
@@ -43,6 +43,19 @@ app.get('/users/:id',async (req,res) => {
     }
 })
 
+//Update User
+app.patch('/users/:id',async (req,res)=>{
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,{new: true,validators:true})
+        if(!user)
+            return res.status(404).send()
+        res.send(user)
+    } catch (e) {
+        return res.status(500).send(e)
+    }
+    
+})
+
 //Create Tasks
 app.post("/tasks", async (req,res) =>{
     //initialise new task
